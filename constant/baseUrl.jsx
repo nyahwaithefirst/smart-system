4eafbaeb6909255747de631aab0733f48eab8160
nyahwaithefirst <nyahwailloyd10@gmail.com>
2025-11-24 18:10:34 +0200

    Changed Base Url

--- constant/baseUrl.jsx
+++ constant/baseUrl.jsx
@@ -1,1 +1,2 @@
-export const serviceBaseUrl = "http://localhost:8095/api/v1/";
+export const serviceBaseUrl = "http://66.29.138.251/:8095/api/v1/";
+// export const serviceBaseUrl = "http://localhost:8095/api/v1/";
