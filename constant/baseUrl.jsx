7fb5d0d2108253d051e2d633e06bebec24b8570d
nyahwaithefirst <nyahwailloyd10@gmail.com>
2025-11-25 04:51:03 +0200

    New Update Fix

--- constant/baseUrl.jsx
+++ constant/baseUrl.jsx
@@ -1,2 +1,2 @@
-export const serviceBaseUrl = "http://66.29.138.251:8095/api/v1/";
+export const serviceBaseUrl = "http://66.29.138.251:8096/api/v1/";
 // export const serviceBaseUrl = "http://localhost:8095/api/v1/";
